388385e7fcfc7185d993c2692ba6f50555b9ec19
Jari Luukkonen <Dragonius@users.noreply.github.com>
2022-04-22 13:30:46 +0300

    Update DCSServerStats.sql

--- mysql/DCSServerStats.sql
+++ mysql/DCSServerStats.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
--- Verkkotietokone:              192.168.2.20
--- Palvelinversio:               10.3.21-MariaDB-log - MariaDB Server
+-- Verkkotietokone:              192.168.2.29
+-- Palvelinversio:               10.5.13-MariaDB - MariaDB Server
 -- Server OS:                    Linux
 -- HeidiSQL Versio:              10.2.0.5599
 -- --------------------------------------------------------
@@ -12,13 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for DCSServerStats
-DROP DATABASE IF EXISTS `DCSServerStats`;
-CREATE DATABASE IF NOT EXISTS `DCSServerStats` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `DCSServerStats`;
+-- Dumping database structure for dcstat
+CREATE DATABASE IF NOT EXISTS `dcstat` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `dcstat`;
 
--- Dumping structure for taulu DCSServerStats.aircrafts
-DROP TABLE IF EXISTS `aircrafts`;
+-- Dumping structure for taulu dcstat.aircrafts
 CREATE TABLE IF NOT EXISTS `aircrafts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -32,15 +30,11 @@ CREATE TABLE IF NOT EXISTS `aircrafts` (
   `inc_hits` int(11) NOT NULL DEFAULT 0,
   `inc_kills` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.aircrafts: ~0 rows (suunnilleen)
-DELETE FROM `aircrafts`;
-/*!40000 ALTER TABLE `aircrafts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `aircrafts` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
--- Dumping structure for taulu DCSServerStats.dcs_events
-DROP TABLE IF EXISTS `dcs_events`;
+-- Dumping structure for taulu dcstat.dcs_events
 CREATE TABLE IF NOT EXISTS `dcs_events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` bigint(20) NOT NULL,
@@ -57,32 +51,24 @@ CREATE TABLE IF NOT EXISTS `dcs_events` (
   `TargetCoa` varchar(255) NOT NULL,
   `TargetGroupCat` varchar(255) NOT NULL,
   `TargetType` varchar(255) NOT NULL,
-  `TargetPlayer` varchar(255) DEFAULT NULL,
+  `TargetPlayer` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=346 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.dcs_events: ~0 rows (suunnilleen)
-DELETE FROM `dcs_events`;
-/*!40000 ALTER TABLE `dcs_events` DISABLE KEYS */;
-/*!40000 ALTER TABLE `dcs_events` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
--- Dumping structure for taulu DCSServerStats.dcs_parser_log
-DROP TABLE IF EXISTS `dcs_parser_log`;
+-- Dumping structure for taulu dcstat.dcs_parser_log
 CREATE TABLE IF NOT EXISTS `dcs_parser_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` int(11) NOT NULL,
   `durationms` int(11) NOT NULL,
   `events` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=307 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.dcs_parser_log: ~0 rows (suunnilleen)
-DELETE FROM `dcs_parser_log`;
-/*!40000 ALTER TABLE `dcs_parser_log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `dcs_parser_log` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
--- Dumping structure for taulu DCSServerStats.flights
-DROP TABLE IF EXISTS `flights`;
+-- Dumping structure for taulu dcstat.flights
 CREATE TABLE IF NOT EXISTS `flights` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pilotid` int(11) NOT NULL,
@@ -96,15 +82,11 @@ CREATE TABLE IF NOT EXISTS `flights` (
   `endofflighttype` varchar(255) NOT NULL DEFAULT 'landing',
   `raw_id` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.flights: ~0 rows (suunnilleen)
-DELETE FROM `flights`;
-/*!40000 ALTER TABLE `flights` DISABLE KEYS */;
-/*!40000 ALTER TABLE `flights` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
--- Dumping structure for taulu DCSServerStats.hitsshotskills
-DROP TABLE IF EXISTS `hitsshotskills`;
+-- Dumping structure for taulu dcstat.hitsshotskills
 CREATE TABLE IF NOT EXISTS `hitsshotskills` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `initiatorPid` int(11) NOT NULL,
@@ -117,17 +99,13 @@ CREATE TABLE IF NOT EXISTS `hitsshotskills` (
   `targetCoa` varchar(255) NOT NULL DEFAULT '',
   `weaponid` int(11) NOT NULL,
   `type` varchar(255) NOT NULL,
-  `target_raw_id` int(11) DEFAULT NULL,
+  `target_raw_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.hitsshotskills: ~0 rows (suunnilleen)
-DELETE FROM `hitsshotskills`;
-/*!40000 ALTER TABLE `hitsshotskills` DISABLE KEYS */;
-/*!40000 ALTER TABLE `hitsshotskills` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
--- Dumping structure for taulu DCSServerStats.pilots
-DROP TABLE IF EXISTS `pilots`;
+-- Dumping structure for taulu dcstat.pilots
 CREATE TABLE IF NOT EXISTS `pilots` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -145,15 +123,11 @@ CREATE TABLE IF NOT EXISTS `pilots` (
   `online` tinyint(1) NOT NULL DEFAULT 0,
   `show_kills` tinyint(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.pilots: ~0 rows (suunnilleen)
-DELETE FROM `pilots`;
-/*!40000 ALTER TABLE `pilots` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pilots` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
--- Dumping structure for taulu DCSServerStats.pilot_aircrafts
-DROP TABLE IF EXISTS `pilot_aircrafts`;
+-- Dumping structure for taulu dcstat.pilot_aircrafts
 CREATE TABLE IF NOT EXISTS `pilot_aircrafts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pilotid` int(11) NOT NULL,
@@ -168,15 +142,11 @@ CREATE TABLE IF NOT EXISTS `pilot_aircrafts` (
   `inc_hits` int(11) NOT NULL DEFAULT 0,
   `inc_kills` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.pilot_aircrafts: ~0 rows (suunnilleen)
-DELETE FROM `pilot_aircrafts`;
-/*!40000 ALTER TABLE `pilot_aircrafts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pilot_aircrafts` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
--- Dumping structure for taulu DCSServerStats.position_data
-DROP TABLE IF EXISTS `position_data`;
+-- Dumping structure for taulu dcstat.position_data
 CREATE TABLE IF NOT EXISTS `position_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pilotid` int(11) NOT NULL,
@@ -188,15 +158,11 @@ CREATE TABLE IF NOT EXISTS `position_data` (
   `missiontime` int(11) NOT NULL,
   `raw_id` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1121 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.position_data: ~0 rows (suunnilleen)
-DELETE FROM `position_data`;
-/*!40000 ALTER TABLE `position_data` DISABLE KEYS */;
-/*!40000 ALTER TABLE `position_data` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
--- Dumping structure for taulu DCSServerStats.weapons
-DROP TABLE IF EXISTS `weapons`;
+-- Dumping structure for taulu dcstat.weapons
 CREATE TABLE IF NOT EXISTS `weapons` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
@@ -205,12 +171,9 @@ CREATE TABLE IF NOT EXISTS `weapons` (
   `hits` int(11) NOT NULL DEFAULT 0,
   `kills` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Dumping data for table DCSServerStats.weapons: ~0 rows (suunnilleen)
-DELETE FROM `weapons`;
-/*!40000 ALTER TABLE `weapons` DISABLE KEYS */;
-/*!40000 ALTER TABLE `weapons` ENABLE KEYS */;
+-- Tietojen vientiä ei oltu valittu.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
