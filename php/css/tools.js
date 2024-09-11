6064027fb330ca085cf44b9748e96d35a46fb1cc
mahaupt <subcortexx@gmail.com>
2024-09-11 13:02:20 +0200

    update tools.js

--- php/css/tools.js
+++ php/css/tools.js
@@ -1,6 +1,6 @@
 var map;
 var markers = [];
-var mapCenter = {lat: 38.18638677, lng: -115.16967773};
+var mapCenter = [38.18638677, -115.16967773];
 var mapZoom = 7;
 var flightId = -1;
 
@@ -63,20 +63,6 @@ function timer() {
 	}
 }
 
-
-function flushMapChanges() {
-	map.panTo(mapCenter);
-	map.setZoom(mapZoom);
-}
-
-function setMapCenter(center) {
-	mapCenter = center;
-}
-
-function setMapZoom(zoom) {
-	mapZoom = zoom;
-}
-
 function setFlightId(fid) {
 	flightId = fid;
 }
@@ -92,42 +78,12 @@ function initFlightPathMap() {
 }
 
 function initMap() {
-	// Specify features and elements to define styles.
-	var customMapType = new google.maps.StyledMapType([
-	{
-		featureType: "all",
-		stylers: [
-			{ saturation: -80 }
-		]
-	},{
-		featureType: "road.arterial",
-		elementType: "geometry",
-		stylers: [
-			{ hue: "#00ffee" },
-			{ saturation: 50 }
-			]
-	},{
-		featureType: "poi.business",
-		elementType: "labels",
-		stylers: [
-			{ visibility: "off" }
-		]
-	}
-	], {
-		name: 'Radar'
-	});
-	var customMapTypeId = 'custom_style';
-	
-	//create map
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: mapCenter,
-		zoom: mapZoom,
-		mapTypeControlOptions: {
-			mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
-		}
-	});
-	map.mapTypes.set(customMapTypeId, customMapType);
-	map.setMapTypeId(customMapTypeId);
+	map = L.map('map').setView(mapCenter, mapZoom);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		minZoom: 0,
+		maxZoom: 17,
+		attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors',
+		}).addTo(map);
 }
 
 
@@ -168,7 +124,8 @@ function loadFlightPathMap() {
 
 
 function addMapMarker(lat, lng, name, text, pathline, show_always) {
-	var infowindow = new google.maps.InfoWindow({
+	// TODO: update this function to leaflet
+	/*var infowindow = new google.maps.InfoWindow({
     	content: text
   	});
   	
@@ -202,19 +159,23 @@ function addMapMarker(lat, lng, name, text, pathline, show_always) {
 	  	flightPath.setMap(map);
   	}
   	
-  	markers.push(marker);
+  	markers.push(marker);*/
 }
 
 
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
-	for (var i = 0; i < markers.length; i++) {
+	// TODO: update this function to leaflet
+	/*for (var i = 0; i < markers.length; i++) {
 		markers[i].setMap(map);
-	}
+	}*/
 }
 
 // Deletes all markers in the array by removing references to them.
 function deleteMarkers() {
+	// TODO: update this function to leaflet
+	/*
 	setMapOnAll(null);
 	markers = [];
+	*/
 }
